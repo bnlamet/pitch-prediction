959c297f8e159230ed6f965d76684f86ae54c6f6
Timm Allman <tallman@cs.umass.edu>
2016-12-17 11:32:43 -0500

    pylint main.py

--- calibration_curve.py
+++ calibration_curve.py
@@ -31,14 +31,15 @@ def plot_curve(labels: list, probas: np.array, actuals: np.array, buckets: int):
     assert probas.min() >= 0 and probas.max() <= 1, 'probas should be real probabilities'
 
     plt.plot([0, 1], [0, 1], 'k:', label='Perfectly calibrated')
-    plt.ylabel("Fraction of positives")
-    plt.ylim([-0.05, 1.05])
-    plt.title('Calibration plots (reliability curve)')
     for i in range(0, d):
         y_prob = probas[i]
         y_true = (actuals == i)
         prob_true, prob_pred = calibration_curve(y_true, y_prob, n_bins=buckets)
         plt.plot(prob_true, prob_pred, ".", label=labels[i])
+
+    plt.ylabel("Fraction of positives")
+    plt.ylim([-0.05, 1.05])
+    plt.title('Calibration plots (reliability curve)')
     plt.legend(loc="lower right")
 
     plt.show()
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import argparse
-from model1 import SimpleCategorical, GaussianMixtureModel
-from model2 import CategoricalNeuralNetwork, MixtureDensityNetwork
-from logistic import Logistic
-import pandas as pd
-from sklearn.model_selection import train_test_split
 import sys
 import itertools
 import random
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from logistic import Logistic
+
+from model1 import SimpleCategorical, GaussianMixtureModel
+from model2 import CategoricalNeuralNetwork, MixtureDensityNetwork
+
 
 def random_hidden_layers():
     uniforms = [np.random.uniform(0.0, 3.0)]
@@ -21,7 +22,9 @@ def model2_find_structure(train, test):
     for _ in range(100):
         hidden_layers = random_hidden_layers()
         print(hidden_layers)
-        model = CategoricalNeuralNetwork(learning_rate = 1.0, batch_size = 2000, sweeps = 25, player_embedding = 40, hidden_layers = hidden_layers, dropout = 0.1)
+        model = CategoricalNeuralNetwork(learning_rate=1.0, batch_size=2000, sweeps=25,
+                                         player_embedding=40, hidden_layers=hidden_layers,
+                                         dropout=0.1)
         model.fit(train)
         loglike = model.log_likelihood(test)
         print(loglike)
@@ -32,7 +35,7 @@ def model1_hyperopt(train, test):
     hypers = list(itertools.product(alphas, components))
     random.shuffle(hypers)
     for hyper in hypers:
-        model = GaussianMixtureModel(alpha = hyper[0], components = hyper[1])
+        model = GaussianMixtureModel(alpha=hyper[0], components=hyper[1])
         model.fit(train)
         loglike = model.log_likelihood(test)
         print(hyper, loglike)
@@ -42,78 +45,71 @@ def model2_hyperopt(train, test):
     batch_sizes = [500, 1000, 2000, 5000]
     player_embeddings = [20, 30, 40]
     sweepss = [100]
-    hidden_layerss = [[100,75,50]] #[[50], [75], [100], [100,50], [100,75], [100,75,50]]
+    hidden_layerss = [[100, 75, 50]] #[[50], [75], [100], [100,50], [100,75], [100,75,50]]
     dropouts = [0.1, 0.2, 0.3, 0.4, 0.5]
-    hypers = list(itertools.product(learning_rates, batch_sizes, player_embeddings, sweepss, hidden_layerss, dropouts))
+    hypers = list(itertools.product(learning_rates, batch_sizes, player_embeddings, sweepss,
+                                    hidden_layerss, dropouts))
     random.shuffle(hypers)
     print('Running Experiments')
     for hyper in hypers:
-        model = CategoricalNeuralNetwork(learning_rate = hyper[0],
-                                            batch_size = hyper[1],
-                                            sweeps = hyper[3],
-                                            player_embedding = hyper[2],
-                                            hidden_layers = hyper[4],
-                                            dropout = hyper[5])
+        model = CategoricalNeuralNetwork(learning_rate=hyper[0],
+                                         batch_size=hyper[1],
+                                         sweeps=hyper[3],
+                                         player_embedding=hyper[2],
+                                         hidden_layers=hyper[4],
+                                         dropout=hyper[5])
         model.fit(train)
         loglike = model.log_likelihood(test)
         print(hyper, loglike)
 
-def model2_randsearch(train, test, tests=100):
-    for _ in range(tests):
-        learning_rate = 0
-        batch_size = 0
-        player_embedding = 0
-        sweeps = 100
-        hidden_layers = [100,75,50]
-        dropout = 0.0
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data', default='data.csv') # already preprocessed data
-    parser.add_argument('--model', choices=['model1', 'model2', 'logistic'], default='model1')
-    parser.add_argument('--sample', default=None, type=int)
-    parser.add_argument('--seed', type=int, default=None)
-    parser.add_argument('--predict', choices=['ptype', 'ploc'], default='ptype')
-    parser.add_argument('--gridsearch', action='store_true')
-    parser.add_argument('--batch_size', type=int, default=2000)
-    parser.add_argument('--dropout', type=float, default=0.1)
-    parser.add_argument('--mixture_components', type=int, default=10)
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('--data', default='data.csv') # already preprocessed data
+    PARSER.add_argument('--model', choices=['model1', 'model2', 'logistic'], default='model1')
+    PARSER.add_argument('--sample', default=None, type=int)
+    PARSER.add_argument('--seed', type=int, default=None)
+    PARSER.add_argument('--predict', choices=['ptype', 'ploc'], default='ptype')
+    PARSER.add_argument('--gridsearch', action='store_true')
+    PARSER.add_argument('--batch_size', type=int, default=2000)
+    PARSER.add_argument('--dropout', type=float, default=0.1)
+    PARSER.add_argument('--mixture_components', type=int, default=10)
 
-    args = parser.parse_args()
+    ARGS = PARSER.parse_args()
 
-    pitches = pd.read_csv(args.data)
+    PITCHES = pd.read_csv(ARGS.data)
 
-    if args.sample:
-        pitches = pitches.sample(args.sample, random_state=args.seed)
+    if ARGS.sample:
+        PITCHES = PITCHES.sample(ARGS.sample, random_state=ARGS.seed)
 
-    train, test = train_test_split(pitches, random_state=args.seed)
+    TRAIN, TEST = train_test_split(PITCHES, random_state=ARGS.seed)
 
-    if args.gridsearch:
-        if args.model == 'model1':
-            model1_hyperopt(train, test)
-        if args.model == 'model2':
-            model2_find_structure(train, test)
+    if ARGS.gridsearch:
+        if ARGS.model == 'model1':
+            model1_hyperopt(TRAIN, TEST)
+        if ARGS.model == 'model2':
+            model2_find_structure(TRAIN, TEST)
 #            model2_hyperopt(train, test)
         sys.exit()
 
-    if args.model == 'model1':
-        if args.predict == 'ptype':
-            model = SimpleCategorical()
-        elif args.predict == 'ploc':
-            model = GaussianMixtureModel() # -2.62724153634
-    elif args.model == 'model2':
-        if args.predict == 'ptype':
-#            model = CategoricalNeuralNetwork(learning_rate = 0.1, batch_size = 1000, sweeps=100, player_embedding = 40, hidden_layers = [250, 300, 100, 60, 33], dropout = 0.1)
-            model = CategoricalNeuralNetwork(show_progress=True)
-        elif args.predict == 'ploc':
-            print("{'mixture_components' : %d, 'batch_size' : %d, 'dropout' : %f}" % (args.mixture_components, args.batch_size, args.dropout))
-            model = MixtureDensityNetwork(mixture_components=args.mixture_components, hidden_layers=[256, 128, 64], batch_size=args.batch_size, player_embedding=50, dropout=args.dropout, show_progress=True)
-    elif args.model == 'logistic':
-        assert args.predict == 'ptype'
-        model = Logistic()
+    if ARGS.model == 'model1':
+        if ARGS.predict == 'ptype':
+            MODEL = SimpleCategorical()
+        elif ARGS.predict == 'ploc':
+            MODEL = GaussianMixtureModel() # -2.62724153634
+    elif ARGS.model == 'model2':
+        if ARGS.predict == 'ptype':
+            MODEL = CategoricalNeuralNetwork(show_progress=True)
+        elif ARGS.predict == 'ploc':
+            MODEL = MixtureDensityNetwork(mixture_components=ARGS.mixture_components,
+                                          hidden_layers=[256, 128, 64], batch_size=ARGS.batch_size,
+                                          player_embedding=50, dropout=ARGS.dropout,
+                                          show_progress=True)
+    elif ARGS.model == 'logistic':
+        assert ARGS.predict == 'ptype'
+        MODEL = Logistic()
 
-    model.fit(train)
-    print('Training Log Likelihood: ', model.log_likelihood(train))
-    print('Testing Log Likelihood: ', model.log_likelihood(test))
-    if args.predict == 'ptype':
-        model.calibration_curve(test)
+    MODEL.fit(TRAIN)
+    print('Training Log Likelihood: ', MODEL.log_likelihood(TRAIN))
+    print('Testing Log Likelihood: ', MODEL.log_likelihood(TEST))
+    if ARGS.predict == 'ptype':
+        MODEL.calibration_curve(TEST)
